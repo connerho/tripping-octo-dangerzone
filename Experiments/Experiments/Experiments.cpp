8e22d93f9618b04b95953744bec87922348a3473
Conner Howell <connerho@gmail.com>
2014-03-05 12:35:14 -0500

    More experimenting
    added struct Experiment to test out the GetSize() function

--- Experiments/Experiments/Experiments.cpp
+++ Experiments/Experiments/Experiments.cpp
@@ -3,9 +3,43 @@
 
 #include "stdafx.h"
 
+struct Experiment
+{
+	int foo;
+	char bar;
+	double baz;
+	short bil;
+};
+
+bool IsEqual(const int &x, const int &y)
+{
+	return !(x ^ y);
+}
+
+template <typename T>
+size_t GetSize(const T &object)
+{
+	size_t retVal;
+	retVal = ((size_t)(&(object) + 1) - (size_t)(&(object)));
+	return retVal;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int testObject1;
+	Experiment testObject2;
+	bool test1 = IsEqual(0x80000000, 0x7fffffff);
+	bool test2 = IsEqual(0x80000000, 0x80000000);
+	bool test3 = (GetSize(testObject1) == sizeof(testObject1));
+	bool test4 = (GetSize(testObject2) == sizeof(testObject2));
+	printf("Test 1: %s \n", ((!test1) ? "PASS" : "FAIL"));
+	printf("Test 2: %s \n", (test2 ? "PASS" : "FAIL"));
+	printf("Test 3: %s \n", (test3 ? "PASS" : "FAIL"));
+	printf("Test 4: %s \n", (test4 ? "PASS" : "FAIL"));
+
+	printf("%ld \n", GetSize(testObject2));
+	printf("%ld \n", sizeof(testObject2));
+
 	return 0;
 }
 
