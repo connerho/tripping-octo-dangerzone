0b4215c77f86196a072ace41e5fbaf5325659c17
Conner Howell <connerho@gmail.com>
2024-03-09 21:27:10 -0500

    Save progress

--- HerpsCoolAddon/main.lua
+++ HerpsCoolAddon/main.lua
@@ -1,57 +1,82 @@
 SLASH_ROLLCALL1 = "/rollcall"
-SLASH_HELLOW1 = "/helloworld"
-SLASH_HELLOW2 = "/hw"
+SLASH_WORLDBUFF1 = "/addwb"
+
+-- Let's define our buffs that we care about here
+local buffs_we_care_about = {
+    ["Elixir of Agility"]           = 5,
+    ["Elixir of Ogre Strength"]     = 5,
+    ["Elixir of Greater Defense"]   = 5,
+    ["Elixir of Frost Power"]       = 5,
+    ["Elixir of Firepower"]         = 5,
+    ["Elixir of Fortitude"]         = 5,
+    ["Demon Skin"]                  = 5,
+    ["Blood Pact"]                  = 5,
+}
+
+-- And define world buffs that we care about too 
+local world_buffs = {
+    ["Boon of Blackfathom"]         = 5,
+    ["Spark of Inspiration"]        = 5,
+}
+
+local function loopThroughMembersAddPoints(table)
+    -- This is how we loop through all of the buff list
+    -- Loop through all members in the raid
+    local numGroupMembers = GetNumGroupMembers();
+    for i = 1, MAX_RAID_MEMBERS do
+        if (i <= numGroupMembers) then
+            local total_ep = 0
+            name, rank, subgroup = GetRaidRosterInfo(i);
+            -- Loop through all of the buffs that we care about
+            for k, v in pairs(table) do
+                local return_val = AuraUtil.FindAuraByName(k, name)
+                if( return_val == nil ) then
+                    print(name,"does not have buff",k)
+                else
+                    -- Player does have the buff, let's keep track of additional EP
+                    print(name,"does have buff", k, "adding EP")
+                    total_ep = total_ep + v
+                end
+            end
+            print(name,"gets", total_ep, "for having buffs")
+        end
+    end
+end
+
+SlashCmdList.WORLDBUFF = function(msg, editBox)
+    loopThroughMembersAddPoints(world_buffs)
+end
 
 -- define the corresponding slash command handler
 SlashCmdList.ROLLCALL = function(msg, editBox)
-    AuraUtil.ForEachAura("player", "HELPFUL", nil, function(name, icon, ...)
-        print(name, icon, ...)
-    end)
-end
-
--- define the corresponding slash command handler
-SlashCmdList.HELLOW = function(msg, editBox)
-	local name1, name2 = strsplit(" ", msg)
-	if #name1 > 0 then -- check for empty string
-		print(format("hello %s and also %s", name1, name2 or "Carol"))
-	else
-		print("Please give at least one name")
-	end
-end
-
-local function buffCallback(name, icon, _, _, _, _, _, _, _, spellId, ...)
-    -- Check to see if any of the buffs matches the ones we care about
-    
-end
-
-local function read_yaml_file(filename)
-    local file = io.open(filename, "r")
-    if not file then
-        return nil, "Failed to open file: " .. filename
-    end
-    local data = {}
-    for line in file:lines() do
-        local key, value = line:match("(%w+):%s*(.+)")
-        if key and value then
-            data[key] = value
+    -- This is how we loop through all of the buff list
+    -- Loop through all members in the raid
+    local numGroupMembers = GetNumGroupMembers();
+    for i = 1, MAX_RAID_MEMBERS do
+        if (i <= numGroupMembers) then
+            local total_ep = 0
+            name, rank, subgroup = GetRaidRosterInfo(i);
+            -- Loop through all of the buffs that we care about
+            for k, v in pairs(buffs_we_care_about) do
+                local return_val = AuraUtil.FindAuraByName(k, name)
+                if( return_val == nil ) then
+                    print(name,"does not have buff",k)
+                else
+                    -- Player does have the buff, let's keep track of additional EP
+                    print(name,"does have buff", k, "adding EP")
+                    total_ep = total_ep + v
+                end
+            end
+            print(name,"gets", total_ep, "for having buffs")
         end
     end
-    file:close()
-    return data
 end
 
 --[[ Loop through all of the players int he raid ]]
 local function loop_through_raid_members()
     local buffFound = 0
-    BUFF_VALUE_PAIRING = read_yaml_file('buffs.yaml')
     for i = 1, MAX_RAID_MEMBERS do
         local unit = format("%s%i", 'raid', i)
         AuraUtil.ForEachAura(unit, "HELPFUL", nil, buffCallback)
     end
 end
-
-read_yaml_file('buffvalues.yaml')
-
-AuraUtil.ForEachAura("player", "HELPFUL", nil, function(name, icon, ...)
-    print(name, icon, ...)
-end)
---
+++ HerpsCoolAddon/moveStuff.ps1
@@ -0,0 +1,3 @@
+cp buffvalues.yaml "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\HerpsCoolAddon"
+cp HerpsCoolAddon.toc "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\HerpsCoolAddon"
+cp main.lua "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\HerpsCoolAddon"
